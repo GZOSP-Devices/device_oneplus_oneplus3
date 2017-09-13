370bc23a99dc9a558e4372be5f26a90aff3c2a16
Martinusbe <martinusbe@gmail.com>
2017-09-13 10:25:58 +0200

    op3: add hal1 camera hal

--- device.mk
+++ device.mk
@@ -159,9 +159,11 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
+    libcamera_shim \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
-    libcamera_shim
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
