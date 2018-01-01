8d2e466c69c8c34e93c8c35046ecf45e2e9f46db
martinusbe <martinusbe@gmail.com>
2018-01-01 11:43:33 +0100

    op3; remove TZ
    Change-Id: I4158478ad66b16a4bef9a4f4d8160f49ddaa35ab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,6 @@ TARGET_SPECIFIC_HEADER_PATH := $(PLATFORM_PATH)/include
 BOARD_VENDOR := oneplus
 
 # Assertions
-TARGET_BOARD_INFO_FILE ?= $(PLATFORM_PATH)/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := OnePlus3,oneplus3,OnePlus3T,oneplus3t
 
 # Use Snapdragon LLVM, if available
