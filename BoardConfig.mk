5f7cd068e9131b029ba26fa118bcfacfe7f125a5
martinusbe <martinusbe@gmail.com>
2018-01-04 15:49:22 +0100

    op3: change defconfig for Akhil's kernel
    Change-Id: I51e74bfe3f5883293db234ea85424360feadebfb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,7 +71,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8996
-TARGET_KERNEL_CONFIG := lineageos_oneplus3_defconfig
+TARGET_KERNEL_CONFIG := oneplus3_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # QCOM hardware
