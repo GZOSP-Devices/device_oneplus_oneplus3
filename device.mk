72be8073809049768fd1fc3ef8abd4d3805ccdda
LuK1337 <priv.luk@gmail.com>
2019-01-27 16:52:48 +0000

    Use vndk-27 android.hardware.gnss@1.0.so

--- device.mk
+++ device.mk
@@ -288,6 +288,9 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.hardware.gnss@1.0-v27.so
+
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
