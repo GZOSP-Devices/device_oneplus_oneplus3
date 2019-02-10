f8654116effa74526146f813fd0bdfbbf386ed33
alliancesparx <alliancesparx@gmail.com>
2019-02-10 08:35:08 +0000

    Device.mk: Don't build oppo_common

--- device.mk
+++ device.mk
@@ -493,4 +493,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
+#$(call inherit-product, device/oppo/common/common.mk)
