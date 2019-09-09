1b15682aec261c9ce4d5f52cd2b2a6999fa0336e
alliancesparx <alliancesparx@gmail.com>
2019-09-09 20:19:38 +0100

    Remove ENG from init.class_late

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -60,7 +60,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.class_late.sh
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.class_late.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
