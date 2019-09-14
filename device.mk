1bd702fb1ad7e619867058f6dfa4562279369cf9
martinusbe <martinusbe@gmail.com>
2019-09-14 11:49:27 +0200

    op3: build ifaa manager

--- device.mk
+++ device.mk
@@ -104,7 +104,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Alipay
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
 PRODUCT_BOOT_JARS += \
