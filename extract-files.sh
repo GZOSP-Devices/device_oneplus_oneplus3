716af8667d67da0607baa716f01ab753f5fe3ca5
Davide Garberi <dade.garberi@gmail.com>
2019-11-17 21:34:47 +0100

    op3: Update vulkan blobs from LA.UM.8.6.r1-01900-89xx.0
    * Older 8x96 blobs break vulkan with updated graphics
    
    Change-Id: I0556b1adf6a62720602f6a4c4994423adb789b1c

--- extract-files.sh
+++ extract-files.sh
@@ -81,3 +81,6 @@ sed -i \
 #
 sed -i "s|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g" \
     "$DEVICE_BLOB_ROOT"/etc/permissions/qti_libpermissions.xml
+
+patchelf --set-soname "vulkan.msm8996.so" "$DEVICE_BLOB_ROOT"/vendor/lib/hw/vulkan.msm8996.so
+patchelf --set-soname "vulkan.msm8996.so" "$DEVICE_BLOB_ROOT"/vendor/lib64/hw/vulkan.msm8996.so
