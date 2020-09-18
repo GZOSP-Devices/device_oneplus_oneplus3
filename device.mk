c78d5f409f15bb9e891b0b5b4741049fc8a61fea
Prema Chand Alugu <premaca@gmail.com>
2020-09-18 11:26:15 +0000

    op3: remove lineage overlay

--- device.mk
+++ device.mk
@@ -28,8 +28,7 @@ $(call inherit-product, vendor/oneplus/oneplus3/oneplus3-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
